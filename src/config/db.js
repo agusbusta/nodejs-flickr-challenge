66477825be22a5820bce98dc4bcdb43ef9e4f903
agusbustadev <agustin.bustamante@chewedpixelstudios.com>
2024-05-24 08:41:55 -0300

    Configure database connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const connection = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: 'password',
-    database: 'flickr'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 module.exports = connection;
